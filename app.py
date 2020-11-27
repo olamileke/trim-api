33b28c60440df50d781e5c368146fd82220c9f96
Fambegbe Olamileke <olamileke.f@gmail.com>
2020-11-27 19:07:58 +0100

    heroku fix

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ api.add_resource(Stats, '/api/stats')
 api.add_resource(PasswordResets, '/api/password/reset')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
